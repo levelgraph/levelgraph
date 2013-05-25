6b00d6fb93ce709c789df3e98184ec8491b985c2
Matteo Collina <matteo.collina@gmail.com>
2013-05-26 00:16:41 +0100

    Query plan generation for a basic, two-conditions case.

--- test/queryplanner_spec.js
+++ test/queryplanner_spec.js
@@ -59,7 +59,7 @@ describe("query planner", function() {
         .yields(null, 10);
 
       stub
-        .withArgs("spo::matteo::friend::", "spo::matteo::friend\xff")
+        .withArgs("pso::friend::matteo::", "pso::friend::matteo\xff")
         .yields(null, 1);
 
       planner(query, function(err, result) {
@@ -90,7 +90,7 @@ describe("query planner", function() {
         .yields(null, 10);
 
       db.approximateSize
-        .withArgs("spo::matteo::friend::", "spo::matteo::friend\xff")
+        .withArgs("pso::friend::matteo::", "pso::friend::matteo\xff")
         .yields(null, 1);
 
       planner(query, function(err, result) {
@@ -109,6 +109,97 @@ describe("query planner", function() {
 
       planner(query, function(err, result) {
         expect(result).to.eql(query);
+        done();
+      });
+    });
+  });
+
+  describe("with sort algorithm", function() {
+
+    buildBefore("sort");
+
+    it("should return a single condition with the JoinStream", function(done) {
+      query = [ { predicate: "friend" } ];
+      expected = [ { predicate: "friend", stream: JoinStream } ];
+
+      stub
+        .withArgs("pos::friend::", "pos::friend\xff", sinon.match.func)
+        .yields(null, 10);
+
+      planner(query, function(err, result) {
+        expect(result).to.eql(expected);
+        done();
+      });
+    });
+
+    it("should use the same index if there is a possible matching", function(done) {
+      query = [{
+          subject: "matteo"
+        , predicate: "friend"
+      }, {
+          predicate: "friend"
+      }];
+
+      expected = [{
+          subject: "matteo"
+        , predicate: "friend"
+        , stream: JoinStream
+        , index: "pso"
+      }, {
+          predicate: "friend"
+        , stream: SortJoinStream
+        , index: "pso"
+      }];
+
+      stub
+        .withArgs("pos::friend::", "pos::friend\xff")
+        .yields(null, 10);
+
+      stub
+        .withArgs("pso::friend::matteo::", "pso::friend::matteo\xff")
+        .yields(null, 1);
+
+      planner(query, function(err, result) {
+        expect(result).to.eql(expected);
+        done();
+      });
+    });
+
+    it("should put the second condition in the same order as the first", function(done) {
+      query = [{
+          subject: v("x")
+        , predicate: "friend"
+        , object: v("c")
+      }, {
+          subject: v("x")
+        , predicate: "abc"
+        , object: v("c")
+      }];
+      
+      expected = [{
+          subject: v("x")
+        , predicate: "friend"
+        , object: v("c")
+        , stream: JoinStream
+        , index: "pos"
+      }, {
+          subject: v("x")
+        , predicate: "abc"
+        , object: v("c")
+        , stream: SortJoinStream
+        , index: "pos"
+      }];
+
+      stub
+        .withArgs("pos::friend::", "pos::friend\xff", sinon.match.func)
+        .yields(null, 1);
+
+      stub
+        .withArgs("pos::abc::", "pos::abc\xff", sinon.match.func)
+        .yields(null, 10);
+
+      planner(query, function(err, result) {
+        expect(result).to.eql(expected);
         done();
       });
     });
