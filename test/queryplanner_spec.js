4f3c7d28a4dcf0c8258aab54e964cce35f503448
Matteo Collina <matteo.collina@gmail.com>
2013-05-25 23:00:31 +0100

    Made more generic the queryplanner test to accomodate more algorithms.

--- test/queryplanner_spec.js
+++ test/queryplanner_spec.js
@@ -7,101 +7,109 @@ describe("query planner", function() {
 
   var db, query, planner, stub, expected;
 
-  beforeEach(function() {
-    db = {
-      approximateSize: function() {}
-    };
-    stub = sinon.stub(db, "approximateSize");
-    planner = queryplanner(db);
-  });
+  function buildBefore(algorithm) {
 
-  it("should return a single condition as-is", function(done) {
-    query = [ { predicate: "friend" } ];
-    expected = [ { predicate: "friend", stream: JoinStream } ];
-
-    stub
-      .withArgs("pos::friend::", "pos::friend\xff", sinon.match.func)
-      .yields(null, 10);
-
-    planner(query, function(err, result) {
-      expect(result).to.eql(expected);
-      done();
+    beforeEach(function() {
+      db = {
+        approximateSize: function() {}
+      };
+      stub = sinon.stub(db, "approximateSize");
+      planner = queryplanner(db, { joinAlgorithm: algorithm });
     });
-  });
+  }
 
-  it("should order two conditions based on their size", function(done) {
-    query = [{
-        subject: "matteo"
-      , predicate: "friend"
-    }, {
-        predicate: "friend"
-    }];
+  describe("with basic algorithm", function() {
 
-    expected = [{
-        subject: "matteo"
-      , predicate: "friend"
-      , stream: JoinStream
-    }, {
-        predicate: "friend"
-      , stream: JoinStream
-    }];
+    buildBefore("basic");
 
-    stub
-      .withArgs("pos::friend::", "pos::friend\xff")
-      .yields(null, 10);
+    it("should return a single condition as-is", function(done) {
+      query = [ { predicate: "friend" } ];
+      expected = [ { predicate: "friend", stream: JoinStream } ];
 
-    stub
-      .withArgs("spo::matteo::friend::", "spo::matteo::friend\xff")
-      .yields(null, 1);
+      stub
+        .withArgs("pos::friend::", "pos::friend\xff", sinon.match.func)
+        .yields(null, 10);
 
-    planner(query, function(err, result) {
-      expect(result).to.eql(expected);
-      done();
+      planner(query, function(err, result) {
+        expect(result).to.eql(expected);
+        done();
+      });
     });
-  });
 
-  it("should order two conditions based on their size (bis)", function(done) {
-    query = [{
-        predicate: "friend"
-    }, {
-        subject: "matteo"
-      , predicate: "friend"
-    }];
+    it("should order two conditions based on their size", function(done) {
+      query = [{
+          subject: "matteo"
+        , predicate: "friend"
+      }, {
+          predicate: "friend"
+      }];
 
-    expected = [{
-        subject: "matteo"
-      , predicate: "friend"
-      , stream: JoinStream
-    }, {
-        predicate: "friend"
-      , stream: JoinStream
-    }];
+      expected = [{
+          subject: "matteo"
+        , predicate: "friend"
+        , stream: JoinStream
+      }, {
+          predicate: "friend"
+        , stream: JoinStream
+      }];
 
-    db.approximateSize
-      .withArgs("pos::friend::", "pos::friend\xff")
-      .yields(null, 10);
+      stub
+        .withArgs("pos::friend::", "pos::friend\xff")
+        .yields(null, 10);
 
-    db.approximateSize
-      .withArgs("spo::matteo::friend::", "spo::matteo::friend\xff")
-      .yields(null, 1);
+      stub
+        .withArgs("spo::matteo::friend::", "spo::matteo::friend\xff")
+        .yields(null, 1);
 
-    planner(query, function(err, result) {
-      expect(result).to.eql(expected);
-      done();
+      planner(query, function(err, result) {
+        expect(result).to.eql(expected);
+        done();
+      });
     });
-  });
 
-  it("should avoid variables", function(done) {
-    query = [ { predicate: "friend", subject: v("x") } ];
-    expected = [ { predicate: "friend", subject: v("x"), stream: JoinStream } ];
+    it("should order two conditions based on their size (bis)", function(done) {
+      query = [{
+          predicate: "friend"
+      }, {
+          subject: "matteo"
+        , predicate: "friend"
+      }];
 
-    stub
-      .withArgs("pos::friend::", "pos::friend\xff", sinon.match.func)
-      .yields(null, 10);
+      expected = [{
+          subject: "matteo"
+        , predicate: "friend"
+        , stream: JoinStream
+      }, {
+          predicate: "friend"
+        , stream: JoinStream
+      }];
 
-    planner(query, function(err, result) {
-      expect(result).to.eql(query);
-      done();
+      db.approximateSize
+        .withArgs("pos::friend::", "pos::friend\xff")
+        .yields(null, 10);
+
+      db.approximateSize
+        .withArgs("spo::matteo::friend::", "spo::matteo::friend\xff")
+        .yields(null, 1);
+
+      planner(query, function(err, result) {
+        expect(result).to.eql(expected);
+        done();
+      });
+    });
+
+    it("should avoid variables", function(done) {
+      query = [ { predicate: "friend", subject: v("x") } ];
+      expected = [ { predicate: "friend", subject: v("x"), stream: JoinStream } ];
+
+      stub
+        .withArgs("pos::friend::", "pos::friend\xff", sinon.match.func)
+        .yields(null, 10);
+
+      planner(query, function(err, result) {
+        expect(result).to.eql(query);
+        done();
+      });
     });
   });
 });
