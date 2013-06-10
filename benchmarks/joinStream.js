42b798b721d88dfa4f5cbd5034c33f0bb788c2fe
Matteo Collina <matteo.collina@gmail.com>
2013-06-10 19:14:41 +0100

    Ported benches to level-test.

--- benchmarks/joinStream.js
+++ benchmarks/joinStream.js
@@ -1,67 +1,59 @@
-var levelup = require("levelup");
-var levelgraph = require("../");
+var level = require("level-test")()
 
-var tmp = require("tmp");
+  , levelgraph = require("../")
 
-tmp.dir(function(err, dir) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
+  , db = levelgraph(level())
 
-  var db = levelgraph(levelup(dir));
+  , startCounts = 100000
+  , counts = startCounts
 
-  var startCounts = 100000;
-  var counts = startCounts;
+  , startTime
+  , endTime
 
-  var startTime;
-  var endTime;
+  , vertexes = ["a", "b"]
 
-  var vertexes = ["a", "b"];
+  , doWrites = function() {
+      if(--counts === 0) {
+        startTime = new Date();
+        doReads();
+        return;
+      }
 
-  var doWrites = function() {
-    if(--counts === 0) {
-      startTime = new Date();
-      doReads();
-      return;
+      var triple = { 
+        subject: vertexes[0],
+        predicate: "p",
+        object: vertexes[1]
+      };
+
+      vertexes.pop();
+      vertexes.unshift("v" + counts);
+
+      db.put(triple, doWrites);
     }
 
-    var triple = { 
-      subject: vertexes[0],
-      predicate: "p",
-      object: vertexes[1]
+  , doReads = function() {
+
+      var stream = db.joinStream([{
+        subject: db.v("a"),
+        predicate: "p",
+        object: db.v("b")
+      }, {
+        subject: db.v("b"),
+        predicate: "p",
+        object: db.v("c")
+      }]);
+
+      stream.on("data", function() {
+        counts++;
+      });
+      stream.on("end", function() {
+        endTime = new Date();
+        var totalTime = endTime - startTime;
+        console.log("total time", totalTime);
+        console.log("total data", counts);
+        console.log("join result/s", startCounts / totalTime * 1000);
+        db.close();
+      });
     };
 
-    vertexes.pop();
-    vertexes.unshift("v" + counts);
-
-    db.put(triple, doWrites);
-  };
-
-  var doReads = function() {
-
-    var stream = db.joinStream([{
-      subject: db.v("a"),
-      predicate: "p",
-      object: db.v("b")
-    }, {
-      subject: db.v("b"),
-      predicate: "p",
-      object: db.v("c")
-    }]);
-
-    stream.on("data", function() {
-      counts++;
-    });
-    stream.on("end", function() {
-      endTime = new Date();
-      var totalTime = endTime - startTime;
-      console.log("total time", totalTime);
-      console.log("total data", counts);
-      console.log("join result/s", startCounts / totalTime * 1000);
-      db.close();
-    });
-  };
-
-  doWrites();
-});
+doWrites();
