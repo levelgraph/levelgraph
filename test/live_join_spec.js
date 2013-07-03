efce118a58aa1b11cc35a58fa014980dcd57fcbb
Matteo Collina <matteo.collina@gmail.com>
2013-07-03 23:31:03 +0100

    Adding one more tests for live joins with two conditions.

--- test/live_join_spec.js
+++ test/live_join_spec.js
@@ -42,4 +42,36 @@ describe("live join algorithm", function() {
       });
     });
   });
+
+  it("should match triples coming from db changes with multiple conditions", function(done) {
+    var contexts = [{ coder: "matteo", friend: "daniele" }]
+        stream = db.joinStream([{ 
+            subject: db.v("coder")
+          , predicate: "does"
+          , object: "code"
+          , live: true
+        }, {
+            subject: db.v("person")
+          , predicate: "friend"
+          , object: db.v("friend")
+        }]);
+
+    stream.on("data", function(data) {
+      expect(data).to.eql(contexts.shift());
+      stream.end();
+    });
+
+    stream.on("end", function() {
+      expect(contexts.length).to.equal(0);
+      done();
+    });
+
+    stream.on("pipe", function() {
+      db.put({
+          subject: "matteo"
+        , predicate: "does"
+        , object: "code"
+      });
+    });
+  });
 });
